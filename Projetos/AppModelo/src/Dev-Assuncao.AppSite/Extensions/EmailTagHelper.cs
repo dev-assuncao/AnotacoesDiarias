e054b1a27fa04d7c3285f761576e65fc96406550
dev-assuncao <davisson.assuncao@protonmail.com>
2021-09-22 23:59:49 -0300

    Working with custom taghelpers

---
+++ Projetos/AppModelo/src/Dev-Assuncao.AppSite/Extensions/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Dev_Assuncao.AppSite.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "gmail.com";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
