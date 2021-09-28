d83037711a4c28d25603f367f89f3359d5b441f0
dev-assuncao <davisson.assuncao@protonmail.com>
2021-09-27 23:06:53 -0300

    types of taghelpers

--- Projetos/AppModelo/src/Dev-Assuncao.AppSite/Extensions/EmailTagHelper.cs
+++ Projetos/AppModelo/src/Dev-Assuncao.AppSite/Extensions/EmailTagHelper.cs
@@ -5,11 +5,14 @@ namespace Dev_Assuncao.AppSite.Extensions
 {
     public class EmailTagHelper : TagHelper
     {
+
+        public string EmailDomain { get; set; } = "outlook.com";
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + "gmail.com";
+            var target = content.GetContent() + "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto:" + target);
             output.Content.SetContent(target);
         }
