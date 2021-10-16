f4aed30bb3799c5377a46793722e762d2cc0af79
dev-assuncao <davisson.assuncao@protonmail.com>
2021-10-15 23:41:00 -0300

    life cicles types of dependency injection

---
+++ Projetos/AppModelo/src/Dev-Assuncao.AppSite/Services/OperacaoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev_Assuncao.AppSite.Services
+{
+    public class OperacaoService
+    {
+    }
+}
