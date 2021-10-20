7f986d2016004bd80109a91afe98170d1090d354
dev-assuncao <davisson.assuncao@protonmail.com>
2021-10-19 23:46:16 -0300

    Crud operations

---
+++ Projetos/AppModelo/src/Dev-Assuncao.AppSite/Migrations/20211020024418_RetirarTeste.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dev_Assuncao.AppSite.Migrations
+{
+    public partial class RetirarTeste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Teste",
+                table: "Alunos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Teste",
+                table: "Alunos",
+                nullable: true);
+        }
+    }
+}
